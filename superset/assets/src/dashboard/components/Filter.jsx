1c3cd40cc881c22e16d61681107ae223c4e42063
Kaylee Edwards <kaylee.edwards@target.com>
2018-09-04 13:19:41 -0500

    Added multiselect boxes to filter

--- superset/assets/src/dashboard/components/Filter.jsx
+++ superset/assets/src/dashboard/components/Filter.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
-import PropTypes from 'prop-types';
+import Select from 'react-select';
 
-const propTypes = {
-  openFilter: PropTypes.bool,
-};
+const propTypes = {};
 
 class Filter extends React.PureComponent {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       openFilter: false,
+      value: [],
     };
 
     this.handleClick = this.handleClick.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+  onInputChange(value) {
+    this.setState({ value });
   }
   handleClick() {
     if (this.state.openFilter) {
@@ -23,6 +26,12 @@ class Filter extends React.PureComponent {
     }
   }
   render() {
+    const options = [
+      { value: 1, label: 'Option1' },
+      { value: 2, label: 'Option2' },
+      { value: 3, label: 'Option3' },
+      { value: 4, label: 'Option4' },
+    ];
     return (
       <div className="filter-container">
         <div className="filter-title">
@@ -39,19 +48,55 @@ class Filter extends React.PureComponent {
           <div className="filter-list">
             <div className="filter-item">
               <div>Filter 1</div>
-              <input />
+              <Select
+                multi
+                defaultValue={options[0]}
+                isClearable
+                isSearchable
+                value={this.state.value}
+                onChange={this.onInputChange}
+                name="test1"
+                options={options}
+              />
             </div>
             <div className="filter-item">
               <div>Filter 2</div>
-              <input />
+              <Select
+                multi
+                defaultValue={options[0]}
+                isClearable
+                isSearchable
+                value={this.state.value}
+                onChange={this.onInputChange}
+                name="test1"
+                options={options}
+              />
             </div>
             <div className="filter-item">
               <div>Filter 3</div>
-              <input />
+              <Select
+                multi
+                defaultValue={options[0]}
+                isClearable
+                isSearchable
+                value={this.state.value}
+                onChange={this.onInputChange}
+                name="test1"
+                options={options}
+              />
             </div>
             <div className="filter-item">
               <div>Filter 4</div>
-              <input />
+              <Select
+                multi
+                defaultValue={options[0]}
+                isClearable
+                isSearchable
+                value={this.state.value}
+                onChange={this.onInputChange}
+                name="test1"
+                options={options}
+              />
             </div>
           </div>
         ) : null}
@@ -60,4 +105,5 @@ class Filter extends React.PureComponent {
   }
 }
 
+Filter.propTypes = propTypes;
 export default Filter;
